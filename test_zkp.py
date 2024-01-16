d5edf4f380f1fe12b72aa045c42af494d6fba353
KosBar49 <34551054+KosBar49@users.noreply.github.com>
2024-01-16 21:55:04 +0100

    Change the name of the transmission participiants

--- test_zkp.py
+++ test_zkp.py
@@ -4,51 +4,51 @@ def test_discrete_log_interactive():
     
     y = 2**5
 
-    proover = DiscreteLogInteractive(2, y, 13, 5)
-    c = proover.commitment()
+    client_a = DiscreteLogInteractive(2, y, 13, 5)
+    c = client_a.commitment()
 
-    verifier = DiscreteLogInteractive(2, y, 13)
-    C = verifier.challenge()
-    res = proover.response(C)
+    client_b = DiscreteLogInteractive(2, y, 13)
+    C = client_b.challenge()
+    res = client_a.response(C)
     
-    verifier.verify(res, c)
+    client_b.verify(res, c)
 
 def test_discrete_log():
-
+    g = 2 # x = 5
     y = 2**5
-    proover = DiscreteLogNonInteractive(2, y, 13, 5)
-    c, V = proover.challenge()
-    r = proover.response()
-    verifier = DiscreteLogNonInteractive(2, y, 13)
-    verifier.verify(r, c, V)
+    client_a = DiscreteLogNonInteractive(2, y, 13, 5)
+    c, V = client_a.challenge()
+    r = client_a.response()
+    client_b = DiscreteLogNonInteractive(2, y, 13)
+    client_b.verify(r, c, V)
     
 def test_discrete_log_ecc():
-    client = DiscreteLogNonInteractiveEcc(5)
-    (t, s) = client.response()
-    proover = DiscreteLogNonInteractiveEcc()
-    proover.verify(t, s)
+    client_a = DiscreteLogNonInteractiveEcc(5)
+    (t, s) = client_a.response()
+    client_b = DiscreteLogNonInteractiveEcc()
+    client_b.verify(t, s)
     
 def test_discrete_log_equality():
 
     y = 2**5
     h = 3**5
 
-    proover = DiscreteLogEqualityNonInteractive(2, y, 3, h, 13, 5)
-    proover.commitments()
-    C = proover.challenge()
-    r = proover.response()
-    verifier = DiscreteLogEqualityNonInteractive(2, y, 3, h, 13)
-    verifier.verify(C, r)
+    client_a = DiscreteLogEqualityNonInteractive(2, y, 3, h, 13, 5)
+    client_a.commitments()
+    C = client_a.challenge()
+    r = client_a.response()
+    client_b = DiscreteLogEqualityNonInteractive(2, y, 3, h, 13)
+    client_b.verify(C, r)
 
 def test_discrete_log_equality_ecc():
     secret = 5
-    client = DiscreteLogEqualityNonInteractiveEcc(secret)
+    client_a = DiscreteLogEqualityNonInteractiveEcc(secret)
     g, h = DiscreteLogEqualityNonInteractiveEcc.curve.get_generators(2)
     P = DiscreteLogEqualityNonInteractiveEcc.curve.scalar_mult(secret, g)
     Q = DiscreteLogEqualityNonInteractiveEcc.curve.scalar_mult(secret, h)
-    (t1, t2, s) = client.response(g, h, P, Q)
-    proover = DiscreteLogEqualityNonInteractiveEcc()
-    proover.verify(g, h, P, Q, t1, t2, s)
+    (t1, t2, s) = client_a.response(g, h, P, Q)
+    client_b = DiscreteLogEqualityNonInteractiveEcc()
+    client_b.verify(g, h, P, Q, t1, t2, s)
 
 def test_discrete_log_conjunction():
     # Example parameters (for demonstration purposes, in practice, use large prime numbers and generators)
@@ -60,73 +60,83 @@ def test_discrete_log_conjunction():
     P = pow(g, a, p)
     Q = pow(h, b, p)
 
-    proover = DiscreteLogConjunction(g, h, P, Q, p, a, b)
-    commitment1, commitment2 = proover.commitment()
-    challenge = proover.challenge()
-    response1, response2 = proover.response()
-    verifier = DiscreteLogConjunction(g, h, P, Q, p)
-    verifier.verify(commitment1, commitment2, response1, response2, challenge)
+    client_a = DiscreteLogConjunction(g, h, P, Q, p, a, b)
+    commitment1, commitment2 = client_a.commitment()
+    challenge = client_a.challenge()
+    response1, response2 = client_a.response()
+    client_b = DiscreteLogConjunction(g, h, P, Q, p)
+    client_b.verify(commitment1, commitment2, response1, response2, challenge)
+
+def test_discrete_log_disjunction():
+    g = 2
+    h = 3
+    p = 17
+    a = 4  # Secret value known to prover
+    P = pow(g, a, p)
+    Q = pow(h, a, p)  # Using the same 'a' for simplicity
+
+    client_a = DiscreteLogDisjunction(g, h, P, Q, p, a)
+    t1c1s1, t2c2s2 = client_a.response()
+    client_b = DiscreteLogDisjunction(g, h, P, Q, p)
+    client_b.verify(g, h, P, Q, t1c1s1, t2c2s2)
     
 def test_discrete_log_conjunction_ecc():
     secret = 5
     secret_2 = 7
-    client = DiscreteLogConjunctionEcc(secret, secret_2)
+    client_a = DiscreteLogConjunctionEcc(secret, secret_2)
     g, h = DiscreteLogConjunctionEcc.curve.get_generators(2)
     P = DiscreteLogConjunctionEcc.curve.scalar_mult(secret, g)
     Q = DiscreteLogConjunctionEcc.curve.scalar_mult(secret_2, h)
-    (t1, s1), (t2, s2) = client.response(g, h, P, Q)
-    proover = DiscreteLogConjunctionEcc()
-    proover.verify(g, h, P, Q, (t1, s1), (t2, s2))
+    (t1, s1), (t2, s2) = client_a.response(g, h, P, Q)
+    client_b = DiscreteLogConjunctionEcc()
+    client_b.verify(g, h, P, Q, (t1, s1), (t2, s2))
+
     
-def test_discrete_log_disjunction():
+def test_discrete_log_disjunction_ecc():
     secret = 5
     b = 7
-    client = DiscreteLogDisjunctionEcc(secret)
+    client_a = DiscreteLogDisjunctionEcc(secret)
     
     g, h = DiscreteLogDisjunctionEcc.curve.get_generators(2)
 
     P = DiscreteLogDisjunctionEcc.curve.scalar_mult(secret, g)
     Q = DiscreteLogDisjunctionEcc.curve.scalar_mult(b, h)
 
-    t1c1s1, t2c2s2 = client.response(g, h, P, Q)
-    proover = DiscreteLogDisjunctionEcc()
-    proover.verify(g, h, P, Q, t1c1s1, t2c2s2)
+    t1c1s1, t2c2s2 = client_a.response(g, h, P, Q)
+    client_b = DiscreteLogDisjunctionEcc()
+    client_b.verify(g, h, P, Q, t1c1s1, t2c2s2)
     
-def test_pederesen_commitment():
+def test_pederesen_commitment_ecc():
     secret = 5
     secret_2 = 7
-    client = PedersenCommitment(secret, secret_2)
+    client_a = PedersenCommitment(secret, secret_2)
     g, h = PedersenCommitment.curve.get_generators(2)
     P = PedersenCommitment.curve.point_add(PedersenCommitment.curve.scalar_mult(secret, g), PedersenCommitment.curve.scalar_mult(secret_2, h))
-    (t, s1, s2) = client.response(g, h, P)
-    proover = PedersenCommitment()
-    proover.verify(g, h, P, t, s1, s2)
+    (t, s1, s2) = client_a.response(g, h, P)
+    client_b = PedersenCommitment()
+    client_b.verify(g, h, P, t, s1, s2)
     
-def test_pederesen_commitment_eq_message_randomness():
+def test_pederesen_commitment_eq_message_randomness_ecc():
     secret = 5
     secret_2 = 7
-    client = PederesenCommitmentEqual(secret, secret_2)
+    client_a = PederesenCommitmentEqual(secret, secret_2)
     g1, h1, g2, h2 = PederesenCommitmentEqual.curve.get_generators(4)
     P = PederesenCommitmentEqual.curve.point_add(PederesenCommitmentEqual.curve.scalar_mult(secret, g1), PederesenCommitmentEqual.curve.scalar_mult(secret_2, h1))
     Q = PederesenCommitmentEqual.curve.point_add(PederesenCommitmentEqual.curve.scalar_mult(secret, g2), PederesenCommitmentEqual.curve.scalar_mult(secret_2, h2))
-    (t1, s1), (t2, s2) = client.response(g1, h1, g2, h2, P, Q)
-    proover = PederesenCommitmentEqual()
-    proover.verify(g1, h1, g2, h2, P, Q, (t1, s1), (t2, s2))
+    (t1, s1), (t2, s2) = client_a.response(g1, h1, g2, h2, P, Q)
+    client_b = PederesenCommitmentEqual()
+    client_b.verify(g1, h1, g2, h2, P, Q, (t1, s1), (t2, s2))
     
-def test_pederesen_commitments():
+def test_pederesen_commitments_ecc():
     secret = 5
     secret2 = 7
     secret3 = 11
     
-    client = PederesenCommitmentsEqual(secret, secret2, secret3)
+    client_a = PederesenCommitmentsEqual(secret, secret2, secret3)
     g1, h1, g2, h2 = PederesenCommitmentsEqual.curve.get_generators(4)
     P = PederesenCommitmentsEqual.curve.point_add(PederesenCommitmentsEqual.curve.scalar_mult(secret, g1), PederesenCommitmentsEqual.curve.scalar_mult(secret2, h1))
     Q = PederesenCommitmentsEqual.curve.point_add(PederesenCommitmentsEqual.curve.scalar_mult(secret, g2), PederesenCommitmentsEqual.curve.scalar_mult(secret3, h2))
     
-    (t1, s1), (t2, s2), s3 = client.response(g1, h1, g2, h2, P, Q)
-    proover = PederesenCommitmentsEqual()
-    proover.verify(g1, h1, g2, h2, P, Q, (t1, s1), (t2, s2), s3)
-
-
-
-    
+    (t1, s1), (t2, s2), s3 = client_a.response(g1, h1, g2, h2, P, Q)
+    client_b = PederesenCommitmentsEqual()
+    client_b.verify(g1, h1, g2, h2, P, Q, (t1, s1), (t2, s2), s3)
