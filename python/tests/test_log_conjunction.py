e48e5556e1148b8ce658d97dd05131483aef7648
Bartosz Kosciug <bartosz.kosciug@gmail.com>
2024-02-21 23:21:22 +0100

    Standarize variables naming.

--- python/tests/test_log_conjunction.py
+++ python/tests/test_log_conjunction.py
@@ -1,50 +1,47 @@
 from ..zkps.zkp_log_conjunction import *
 
 def test_discrete_log_interactive_conjunction():
-    # Example parameters (for demonstration purposes, in practice, use large prime numbers and generators)
     g = 2
     h = 3
-    p = 17  # Large prime for a real implementation
-    a = 4
-    b = 5
-    P = pow(g, a, p)
-    Q = pow(h, b, p)
+    p = 1019 
+    x = 4
+    y = 5
+    P = pow(g, x, p)
+    Q = pow(h, y, p)
 
-    client_a = DiscreteLogConjunctionInteractive(g, h, P, Q, p, a, b)
-    commitment1, commitment2 = client_a.commitment()
-    challenge = client_a.challenge()
-    response1, response2 = client_a.response()
+    client_a = DiscreteLogConjunctionInteractive(g, h, P, Q, p, x, y)
     client_b = DiscreteLogConjunctionInteractive(g, h, P, Q, p)
-    client_b.verify(commitment1, commitment2, response1, response2, challenge)
+    
+    t1, t2 = client_a.commitment()
+    challenge = client_a.challenge()
+    s1, s2 = client_a.response()
+    client_b.verify(t1, t2, s1, s2, challenge)
     
 def test_discrete_log_conjunction():
-    # Example parameters (for demonstration purposes, in practice, use large prime numbers and generators)
     g = 2
     h = 3
-    p = 17  # Large prime for a real implementation
-    a = 4
-    b = 5
-    P = pow(g, a, p)
-    Q = pow(h, b, p)
+    p = 1019
+    x = 4
+    y = 5
+    P = pow(g, x, p)
+    Q = pow(h, y, p)
 
-    # Create an instance of DiscreteLogConjunction with known secret 'a'
-    prover = DiscreteLogConjunction(g, h, P, Q, p, a, b)
-
-    # Generate the response (non-interactive, so no commitment or external challenge)
-    (t1, s1), (t2, s2) = prover.response()
-
-    # Another instance or the same instance can be used for verification
-    # Here, we use the same instance for simplicity
-    prover.verify(g, h, P, Q, (t1, s1), (t2, s2))
+    client_a = DiscreteLogConjunction(g, h, P, Q, p, x, y)
+    client_b = DiscreteLogConjunction(g, h, P, Q, p)
+    
+    (t1, s1), (t2, s2) = client_a.response()
+    client_b.verify(g, h, P, Q, (t1, s1), (t2, s2))
 
     
 def test_discrete_log_conjunction_ecc():
-    secret = 5
-    secret_2 = 7
-    client_a = DiscreteLogConjunctionEcc(secret, secret_2)
-    g, h = DiscreteLogConjunctionEcc.curve.get_generators(2)
-    P = DiscreteLogConjunctionEcc.curve.scalar_mult(secret, g)
-    Q = DiscreteLogConjunctionEcc.curve.scalar_mult(secret_2, h)
-    (t1, s1), (t2, s2) = client_a.response(g, h, P, Q)
+    a = 5
+    b = 7
+    
+    client_a = DiscreteLogConjunctionEcc(a, b)
     client_b = DiscreteLogConjunctionEcc()
+    
+    g, h = DiscreteLogConjunctionEcc.curve.get_generators(2)
+    P = DiscreteLogConjunctionEcc.curve.scalar_mult(a, g)
+    Q = DiscreteLogConjunctionEcc.curve.scalar_mult(b, h)
+    (t1, s1), (t2, s2) = client_a.response(g, h, P, Q)
     client_b.verify(g, h, P, Q, (t1, s1), (t2, s2))
--- python/tests/test_log_discrete.py
+++ python/tests/test_log_discrete.py
@@ -1,25 +1,27 @@
 from ..zkps.zkp_log_discrete import *
 
 def test_discrete_log_interactive():
-    
-    y = 2**5
+    g = 2
+    x = 5
+    P = g**x
 
-    client_a = DiscreteLogInteractive(2, y, 13, 5)
-    c = client_a.commitment()
+    client_a = DiscreteLogInteractive(2, P, 13, 5)
+    t = client_a.commitment()
 
-    client_b = DiscreteLogInteractive(2, y, 13)
-    C = client_b.challenge()
-    res = client_a.response(C)
+    client_b = DiscreteLogInteractive(2, P, 13)
+    c = client_b.challenge()
+    res = client_a.response(c)
     
-    client_b.verify(res, c)
+    client_b.verify(res, t)
 
 def test_discrete_log():
-    g = 2 # x = 5
-    y = 2**5
-    client_a = DiscreteLog(2, y, 13, 5)
-    r, V = client_a.response()
-    client_b = DiscreteLog(2, y, 13)
-    client_b.verify(r, V)
+    g = 2
+    x = 5
+    P = g**x
+    client_a = DiscreteLog(g, P, 13, x)
+    s, t = client_a.response()
+    client_b = DiscreteLog(g, P, 13)
+    client_b.verify(s, t)
     
 def test_discrete_log_ecc():
     client_a = DiscreteLogEcc(5)
